cc6246365c2e29bf25d80b7308a3b4d2e544c0b7
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-30 10:14:29 +0000

    Add summernote to description field

--- totiemb/admin.py
+++ totiemb/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Product)
+class ProductAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
 
 # Register your models here.
+
--- totiemb/models.py
+++ totiemb/models.py
@@ -5,13 +5,13 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 
 
-class Products(models.Model):    
+class Product(models.Model):    
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     maker = models.CharField(max_length=100)
     image = CloudinaryField('image', default='placeholder')
     product_type = models.CharField(max_length=100, blank=True)
-    Description = models.TextField(blank=True)
+    description = models.TextField(blank=True)
     product_size = models.CharField(max_length=50, default="0 ml")
     product_price = models.CharField(max_length=50, default="£0.00")
     created_on = models.DateTimeField(auto_now_add=True)
