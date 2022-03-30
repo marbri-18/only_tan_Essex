08325d686963548f53182aed5a55d8d6f1ac3198
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-30 06:58:18 +0000

    Test commit deployment still working

--- totiemb/models.py
+++ totiemb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, "draft"), (1, "published"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
