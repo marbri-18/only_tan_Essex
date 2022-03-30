0232f784475117d038bbbfb9ae523fee0dd69eba
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-30 23:26:47 +0000

    Configure Django admin panel

--- totiemb/admin.py
+++ totiemb/admin.py
@@ -17,12 +17,23 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('lname', 'fname', 'username', 'dob', 'created_on')
     list_filter = ('created_on', 'is_admin')
     search_fields = ('lname', 'username', 'date_of_birth')
+    actions = ['set_admin']
+
+    def set_admin(self, request, queryset):
+        queryset.update(is_admin=True)
+        
 
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     list_display = ('session_date_time', 'customer_name', 'over_18_yrs', 'tandc_understood')
     list_filter = ('session_date_time', 'customer_name')
+    actions = ['set_over_18_yrs', 'set_tandc_understood']
 
+    def tandc_understood(self, request, queryset):
+        queryset.update(tandc_understood=True)
+
+    def set_over_18_yrs(self, request, queryset):
+        queryset.update(over_18_yrs=True)
 
 
 
