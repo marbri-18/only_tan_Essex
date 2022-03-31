6e9708c0e1efbd09093e469916d3dcba17eb3d97
Martin Bridgman <martin.bridgman@gmail.com>
2022-03-31 20:17:42 +0000

    Set up site main pages structure

---
+++ totiemb/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.contrib import admin
+from django.urls import path
+from totiemb.views import get_home, ProductList, get_events, get_about, get_contact
+
+
+urlpatterns = [
+    path('', get_home, name='home'),
+    path('product', views.ProductList.as_view(), name='product'),
+    path('events', get_events, name='event'),
+    path('about', get_about, name='about'),
+    path('contact', get_contact, name='contact'),
+]
--- totiemb/views.py
+++ totiemb/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
 # Create your views here.
+
+def get_home(request):
+    return render(request,'index.html')
+
+
+class ProductList(generic.ListView):
+    model = Product
+    queryset = Product.objects.order_by('-created_on')
+    template_name = 'products.html'
+    paginate_by = 9
+
+def get_events(request):
+    return render(request,'events.html')
+
+def get_about(request):
+    return render(request,'about.html')
+
+def get_contact(request):
+    return render(request,'contact.html')
